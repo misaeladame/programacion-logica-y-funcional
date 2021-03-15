892314c08732f1cde41a1a894747f347610b1e11
Misael Adame <misael_adame@protonmail.com>
2021-03-15 14:55:37 -0600

    02 funciones

--- Haskell/02.hs
+++ Haskell/02.hs
@@ -2,23 +2,14 @@
   * 01.hs                     *
   * Autor: Yo                 *
   *****************************-}
--- Funcion de suma
-   suma x y = x + y
--- Funcion de resta
-   resta x y = x - y
--- Funcion media aritmetica de tres numeros
-   mediaTres x y z = (x + y + z)/3
--- Funcion doble de un numero
-   doble x = 2*x
--- Funcion potencia de un numero
-   potencia x = x * x
--- Funcion maximo de tres numeros
-   maxTres x y z = max x (max y z)
--- Funcion minimo de tres numeros
-   minTres x y z = min x (min y z)
--- Funcion verifica si tres numeros son iguales
-   igualTres x y z = x == y && y == z
--- Funcion que verifica si tres numeros son diferentes
-   difTres x y z = x/=y && x/=z && y/=z
-   
+
+--Funcion factorial
+factorial :: Integer -> Integer -- representa num muy grandes
+factorial n = product [1..n]
+--Funcion circunferenciaF
+circunferenciaF :: Float -> Float 
+circunferenciaF r = 2 * pi * r
+--Función circunferenciaD
+circunferenciaD :: Double -> Double 
+circunferenciaD r = 2 * pi * r
    
