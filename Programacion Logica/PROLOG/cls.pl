86265df6006008b08d6041ca132211e312cadfeb
Misael Adame <misael_adame@protonmail.com>
2021-05-20 16:11:35 -0500

    Limpia de pantalla y Prog1

---
+++ Programacion Logica/PROLOG/cls.pl
@@ -0,0 +1,6 @@
+%%%%%%%
+% Borrado de pantalla
+%%%%%%%
+%regla unica
+
+cls :- write('\33\[2J').
--- Programacion Logica/PROLOG/uno.pl
+++ Programacion Logica/PROLOG/uno.pl
@@ -1,5 +1,21 @@
 /* SWI-Prolog
    ejemplo de un bloque de comentarios */
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%  uno.pl   extensiones: .pl .pro
+%  Primer programa en Prolog
+%  ** demuestra base de conocimientos
+%  Semestre EJ 2021
+%  Programacion logica y funcional
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 esHombre(juan).
 esHombre(ricardo).
+quiere(juan, lola).
+quiere(juan, ana).
+quiere(ricardo, maria).
+quiere(ricardo, lupe).
+gusta(juan, lola).
+gusta(ricardo, maria).
+esPadre(juan, pedro).
+esPadre("Dios", _).
