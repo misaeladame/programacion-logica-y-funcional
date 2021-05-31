ac9fb27239725019335f60f08fb83e6193bb58fa
Misael Adame <misael_adame@protonmail.com>
2021-05-31 13:16:26 -0500

    Operadores aritmeticos y relacionales

--- Programacion Logica/PROLOG/seis.pl
+++ Programacion Logica/PROLOG/seis.pl
@@ -1,9 +1,31 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %   seis.pl
 %   Programa en Prolog
-%   Ejemplo de operaciones aritméticas
+%   Ejemplo de operaciones aritméticas-relaciones
 %   Semestre EJ 2021
 %   Programacion logica y funcional
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 suma( X, Result) :- X =< 1, Result is 1.
+
+%% Con operaciones aritméticas
+objeto('celular', 6, 12).
+objeto('tablet', 15, 20).
+area(Objeto, D) :-
+    objeto(Objeto, B, A),
+    D is B * A,
+    write(Objeto), write(' : '),
+    write(D).
+
+%% Con operadores relacionales
+maximo(X, Y, X) :- X >= Y.
+maximo(X, Y, Y) :- Y >= X.
+
+libro('C++', autor1, 2020).
+libro('C#', autor1, 2021).
+libro('Java', autor1, 2019).
+libro('JavaScript', autor2, 2019).
+busca(Autor, X) :-
+    libro(Titulo, Autor, Anio),
+    Anio =< X,
+    write(Titulo), nl.
